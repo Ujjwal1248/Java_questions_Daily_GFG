f468ab7240d87c441a7d7a2235b4238db95cb215
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-09-21 19:31:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Largest Subarray of Equal 0's and 1's/largest-subarray-of-equal-0s-and-1s.java
@@ -0,0 +1,20 @@
+class Solution {
+	public int maxLen(int[] arr) {
+		// code here
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int sum = 0;
+		int c = 0;
+		map.put(0, -1);
+		for (int i = 0; i <arr.length; i++) {
+			if (arr[i] == 0)
+				sum += -1;
+			else
+				sum += 1;
+			if (map.containsKey(sum)) {
+				c = Math.max(c, i - map.get(sum));
+			}
+			else map.put(sum, i);
+		}
+		return c;
+	}
+}
